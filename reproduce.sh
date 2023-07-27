75cc3509784230b06e8bba954cbede404b3e74c7
Daniele Baccega <daniele.baccega@unito.it>
2023-07-27 17:48:33 +0200

    Fixed the UID used to run the docker

--- reproduce.sh
+++ reproduce.sh
@@ -7,4 +7,4 @@ if [[ ! -d ptb-xl-a-large-publicly-available-electrocardiography-dataset-1.0.1 ]
 fi
 
 docker build -t danielebaccega/reproduce-cnn-ecg .
-docker run -it --user 1001:1001 -v $(pwd):/home/docker/cnn-ecg danielebaccega/reproduce-cnn-ecg /usr/bin/bash -c "/home/docker/cnn-ecg/run_configurations.sh"
+docker run -it --user $UID:$UID -v $(pwd):/home/docker/cnn-ecg danielebaccega/reproduce-cnn-ecg /usr/bin/bash -c "/home/docker/cnn-ecg/run_configurations.sh"
